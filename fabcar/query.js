6ae9dfa4fa28337ed7da6f4335c8fc59d1f9f06c
snipzer <l.arif@laposte.net>
2018-11-08 16:01:27 +0100

    toto

--- fabcar/query.js
+++ fabcar/query.js
@@ -54,8 +54,8 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
 	const request = {
 		//targets : --- letting this default to the peers assigned to the channel
 		chaincodeId: 'fabcar',
-		fcn: 'queryAllAnnulations',
-		args: ['']
+		fcn: 'createAnnulation',
+		args: ['ANN3', '666', ['CON01', 'CON02']]
 	};
 
 	// send the query proposal to the peer
